bfcd17ca52ac9eae919bae0a7677f0ab8847fffb
Admin <admin@301lt10615.sidom.sidel.com>
2016-11-04 15:13:24 +0100

    Fix rules eslint

--- shared/webpack.config.js
+++ shared/webpack.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname,
-        filename: 'index.js',
-        libraryTarget: 'umd'
-    },
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
-    }
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }],
+  },
 };
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,33 +1,33 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        javascript: [
+  entry: {
+    javascript: [
             // 'babel-polyfill',
-            'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
-            'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors 
-            './src/app.js'
-        ],
-        html: './src/index.html'
-    },
-    resolve: { fallback: path.join(__dirname, "node_modules") },
-    resolveLoader: { fallback: path.join(__dirname, "node_modules") }, 
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        loaders: [{
-            test: /\.html$/,
-            loader: "file?name=[name].[ext]"
-        }, {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ]
+      'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+      'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+      './src/app.js',
+    ],
+    html: './src/index.html',
+  },
+  resolve: { fallback: path.join(__dirname, 'node_modules') },
+  resolveLoader: { fallback: path.join(__dirname, 'node_modules') },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.html$/,
+      loader: 'file?name=[name].[ext]',
+    }, {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
