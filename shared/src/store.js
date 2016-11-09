a8ad2d32b0b93e6157f3e0d44378b2ec39709a82
Quentin Bisson <q.bisson@proxiad.rouen>
2016-11-09 18:53:17 +0100

    Création du reducer

--- shared/src/store.js
+++ shared/src/store.js
@@ -1,10 +1,31 @@
 import { createStore } from 'redux';
 
-const dummyReducer = (state = {}) => state;
+const initialState = {
+  articles: [],
+  error: undefined,
+  url: '',
+};
+
+const articleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SEARCH_ARTICLES_SUCCESS':
+      return {
+        ...state,
+        articles: action.feed.items,
+      };
+    case 'SEARCH_ARTICLES_ERROR':
+      return {
+        ...state,
+        error: action.error, 
+      };
+    default:
+      return state;
+  }
+};
 
 /**
  * Crée le store de notre application.
  */
 export default function () {
-  return createStore(dummyReducer);
+  return createStore(articleReducer);
 }
