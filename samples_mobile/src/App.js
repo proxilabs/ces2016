6582ae3a8d4365d1fd691f350bf32faccac39c2e
Julien Leicher <jleicher@gmail.com>
2016-11-14 07:31:13 +0100

    Ajout samples_mobile

---
+++ samples_mobile/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Text style={{ color: 'red', fontSize: 32 }}>Hello React Native</Text>
+    </View>
+  );
+}
+
+export default App;
